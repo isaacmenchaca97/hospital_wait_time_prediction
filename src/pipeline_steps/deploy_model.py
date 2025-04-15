6e5777ef1d91503c3b30d6c7c9efed30039356ed
Isaac Menchaca <trane9387@hotmail.com>
2025-04-14 19:05:13 -0600

    deploy and register model steps

---
+++ src/pipeline_steps/deploy_model.py
@@ -0,0 +1,48 @@
+from sagemaker.lambda_helper import Lambda
+from sagemaker.workflow.lambda_step import LambdaStep
+
+from src.pipeline_config import SCRIPTS_DIR
+
+
+class DeployModelStep:
+    def __init__(self, pipeline_config, register_step):
+        """Initialize deploy model step
+
+        Args:
+            pipeline_config: Instance of PipelineConfig with configuration
+            regiaster_step: RegisterModel object
+        """
+        self.config = pipeline_config
+        self.register_step = register_step
+
+    def create_lambda_helper(self):
+        """Define Lambda helper class"""
+        self.func = Lambda(
+            function_name="sagemaker-hospital-wait-time-lambda-step",
+            execution_role_arn=self.config.sagemaker_role,
+            script=f"{SCRIPTS_DIR}/lambda_deployer.py",
+            handler="lambda_deployer.lambda_handler",
+            timeout=600,
+            memory_size=10240,
+        )
+
+    def get_lambda_step(self):
+        """Create th lambda step for the pipeline
+
+        Return:
+            LambdaStep: the configured lambda step
+        """
+        self.create_lambda_helper()
+        return LambdaStep(
+            name="LambdaStepRealTimeDeploy",
+            lambda_func=self.func,
+            inputs={
+                "model_name": self.config.pipeline_model_name,
+                "endpoint_config_name": self.config.endpoint_config_name,
+                "endpoint_name": self.config.endpoint_name,
+                "model_package_arn": self.register_step.steps[0].properties.ModelPackageArn,
+                "role": self.config.sagemaker_role,
+                "instance_type": self.config.deploy_instance_type_param,
+                "instance_count": self.config.deploy_instance_count_param,
+            },
+        )
---
+++ src/pipeline_steps/register_model.py
@@ -0,0 +1,32 @@
+from sagemaker.workflow.step_collections import RegisterModel
+
+
+class RegisterModelStep:
+    def __init__(self, pipeline_config, train_step):
+        """Initialize resgister model step
+
+        Args:
+            pipeline_config: Instance of PipelineConfig with configuration
+            train_step: TrainingStep object
+        """
+        self.config = pipeline_config
+        self.train_step = train_step
+
+    def get_register_model_step(self):
+        """Create the resgister model step for the pipeline
+
+        Return:
+            RegisterModel: The configured register model step
+        """
+        return RegisterModel(
+            name="RegisterModel",
+            estimator=self.config.xgb_estimator,
+            # Fetching S3 location where train step saved model artifacts
+            model_data=self.train_step.properties.ModelArtifacts.S3ModelArtifacts,
+            content_types=["text/csv"],
+            response_types=["text/csv"],
+            inference_instances=[self.config.predictor_instance_type],
+            transform_instances=[self.config.predictor_instance_type],
+            model_package_group_name=self.config.model_package_group_name,
+            approval_status=self.config.model_approval_status_param,
+        )
